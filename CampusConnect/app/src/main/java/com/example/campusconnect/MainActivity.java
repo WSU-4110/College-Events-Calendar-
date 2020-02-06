6e3b5b18367eb5365f97ad5a7fcd945e16ac6625
Tesneem Algahim <fx0285@wayne.edu>
2020-02-06 18:43:04 -0500

    Testing commit- Tesneem

--- CampusConnect/app/src/main/java/com/example/campusconnect/MainActivity.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.campusconnect;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+   TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      test= (TextView) findViewById(R.id.testText);
+      test.setText("testing.");
     }
 }
