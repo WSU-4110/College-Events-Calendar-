34329520c80d0c2a11be36beeb4e34aac382a640
Adam-Erdman <dz6416@wayne.edu>
2020-02-13 14:46:23 -0500

    Fixed Manifest xml info for EventCreation.  Builds and runs first page but the link breaks app on use. working to fix

--- CampusConnect/app/src/main/java/com/example/campusconnect/EventCreation.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/EventCreation.java
@@ -1,7 +1,23 @@
 package com.example.campusconnect;
 
-public class EventCreation {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
+public class EventCreation extends AppCompatActivity{
 
+
+    TextView test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_creation);
+
+        test= (TextView) findViewById(R.id.testText);
+        test.setText("testing.");
+        // testing commit and push
+    }
 }
--- CampusConnect/app/src/main/java/com/example/campusconnect/MainActivity.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.campusconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    TextView test;
 
-   TextView test;
+
+//    testing commit and push
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = (TextView) findViewById(R.id.testText);
+        test.setText("testing.");
 
-      test= (TextView) findViewById(R.id.testText);
-      test.setText("testing.");
-      // testing commit and push
+        button = findViewById(R.id.gotoEventCreation);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEventCreator();
+            }
+        });
+
     }
-}
+
+    public void openEventCreator() {
+        Intent intent = new Intent(this, EventCreation.class);
+        startActivity(intent);
+    }
+
+
+
+    }
+
