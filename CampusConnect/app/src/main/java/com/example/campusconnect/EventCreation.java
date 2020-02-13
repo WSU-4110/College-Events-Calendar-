980e8244aaea9edf6bacad2d3e71c79f7f3445c0
Adam-Erdman <dz6416@wayne.edu>
2020-02-13 16:52:05 -0500

    Build Gradle modified for firebase dependencies

--- CampusConnect/app/src/main/java/com/example/campusconnect/EventCreation.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/EventCreation.java
@@ -1,5 +1,6 @@
 package com.example.campusconnect;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,7 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 class Event {
@@ -18,10 +20,18 @@ class Event {
     private String startTime;
     private String date;
 
-    public Event() {}
+//    public Event(){
+//        this.name = "";
+//        this.location = "";
+//        this.startTime = "";
+//        this.date = "";
+//    }
 
     public Event(String name, String location, String startTime, String date) {
-        // ...
+        this.name = name;
+        this.location = location;
+        this.startTime = startTime;
+        this.date = date;
     }
 
     public String getName() {
@@ -54,6 +64,7 @@ public class EventCreation extends AppCompatActivity{
     EditText dateInput;
 
     Button submitButton;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
 
@@ -64,14 +75,16 @@ public class EventCreation extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_creation);
 
-        EventNameInput = (EditText) findViewById(R.id.EventName);
-        locationInput = (EditText) findViewById(R.id.Location);
-        startTimeInput = (EditText) findViewById(R.id.Time);
-        dateInput = (EditText) findViewById(R.id.EventDate);
+
 
 //        submitButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
+//                EventNameInput = (EditText) findViewById(R.id.EventName);
+//                locationInput = (EditText) findViewById(R.id.Location);
+//                startTimeInput = (EditText) findViewById(R.id.Time);
+//                dateInput = (EditText) findViewById(R.id.EventDate);
+
 //                EventName = EventNameInput.getText().toString();
 //                location = locationInput.getText().toString();
 //                startTime = startTimeInput.getText().toString();
