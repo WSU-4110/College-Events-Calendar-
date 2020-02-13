0a818f001217bd662e54cc347c486f5f8ed08b08
Adam-Erdman <dz6416@wayne.edu>
2020-02-13 15:34:55 -0500

    Adding Data gathering to EventCreation.java

--- CampusConnect/app/src/main/java/com/example/campusconnect/EventCreation.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/EventCreation.java
@@ -1,12 +1,25 @@
 package com.example.campusconnect;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class EventCreation extends AppCompatActivity{
+
+    String EventName, location, startTime, date;
+
+    EditText EventNameInput;
+    EditText locationInput;
+    EditText startTimeInput;
+    EditText dateInput;
+
+    Button submitButton;
+
 
 
 
@@ -15,6 +28,20 @@ public class EventCreation extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_creation);
-        
+
+        EventNameInput = (EditText) findViewById(R.id.EventName);
+        locationInput = (EditText) findViewById(R.id.Location);
+        startTimeInput = (EditText) findViewById(R.id.Time);
+        dateInput = (EditText) findViewById(R.id.EventDate);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventName = EventNameInput.getText().toString();
+                location = locationInput.getText().toString();
+                startTime = startTimeInput.getText().toString();
+                date = dateInput.getText().toString();
+            }
+        });
     }
 }
