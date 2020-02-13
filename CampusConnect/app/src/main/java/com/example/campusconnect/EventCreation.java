71b389b87a7eaaaddff565436312d8dbc0d0568f
Adam-Erdman <dz6416@wayne.edu>
2020-02-13 15:06:52 -0500

    Worked on Layout for Event Creation

--- CampusConnect/app/src/main/java/com/example/campusconnect/EventCreation.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/EventCreation.java
@@ -15,7 +15,6 @@ public class EventCreation extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_creation);
-
-
+        
     }
 }
