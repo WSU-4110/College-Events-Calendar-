1b0771ab0c148510472a786116fc591553935b9f
JHuraibi <fh1328@wayne.edu>
2020-02-14 18:05:30 -0500

    Minimal Edits

--- CampusConnect/app/src/main/java/com/example/campusconnect/MainActivity.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/MainActivity.java
@@ -9,52 +9,49 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private Button goto_calendar;
-
-    TextView test;
-
+	private Button button;
+	private Button goto_calendar;
+	
+	TextView test;
 
 
 //    testing commit and push
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        test = (TextView) findViewById(R.id.testText);
-        test.setText("testing.");
-
-        // Button: To Event Creation Page
-        button = findViewById(R.id.gotoEventCreation);                               // [BUTTON ACTION]: Event Creation Page
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openEventCreator();
-            }
-        });
-
-        goto_calendar = findViewById(R.id.goto_calendar);                           // [BUTTON ACTION]: Calendar Month-View
-        goto_calendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                open_View_Calendar();
-            }
-        });
-    }
-
-    public void openEventCreator() {
-        Intent intent = new Intent(this, EventCreation.class);
-        startActivity(intent);
-    }
-
-    public void open_View_Calendar() {
-        Intent intent = new Intent(this, View_Calendar.class);
-        startActivity(intent);
-    }
-
-
-
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		test = (TextView) findViewById(R.id.testText);
+		test.setText("testing.");
+		
+		button = findViewById(R.id.gotoEventCreation);						// [BUTTON ACTION]: Event Creation Page
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				openEventCreator();
+			}
+		});
+		
+		goto_calendar = findViewById(R.id.goto_calendar);					// [BUTTON ACTION]: Calendar View (Month)
+		goto_calendar.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				open_View_Calendar();
+			}
+		});
+	}
+	
+	public void openEventCreator() {
+		Intent intent = new Intent(this, EventCreation.class);
+		startActivity(intent);
+	}
+	
+	public void open_View_Calendar() {
+		Intent intent = new Intent(this, View_Calendar.class);
+		startActivity(intent);
+	}
+	
+	
 }//end [ CLASS: MainActivity ]
 
