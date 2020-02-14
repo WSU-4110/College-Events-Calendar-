0a0fbfa54bf1f5bc740dbe6e7ecc5b76549e4b7a
Adam-Erdman <dz6416@wayne.edu>
2020-02-13 22:12:38 -0500

    Deleting Log-in template

--- CampusConnect/app/src/main/java/com/example/campusconnect/UserSignIn.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/UserSignIn.java
@@ -1,40 +1,14 @@
 package com.example.campusconnect;
 
-
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.AuthResult;
-
-
 
 public class UserSignIn extends AppCompatActivity {
-
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_in);
-
-// Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);//not sure if we need to implement updateUI, currently not implemented
-    }
-
-
 }
