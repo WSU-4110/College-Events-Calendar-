a5f5b038b1efaf983fbcbb74728e4db5272d6ec1
Tesneem Algahim <fx0285@wayne.edu>
2020-02-14 19:34:45 -0500

    Implementing the log-in feature in java code and xml. Need to connect it to the database next and test it. - Tesneem

--- CampusConnect/app/src/main/java/com/example/campusconnect/MainActivity.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/MainActivity.java
@@ -11,8 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 	private Button button;
 	private Button goto_calendar;
+	private Button goto_signin;
 	
-	TextView test;
+	TextView test; // delete later
 
 
 //    testing commit and push
@@ -22,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		test = (TextView) findViewById(R.id.testText);
-		test.setText("testing.");
+		test = (TextView) findViewById(R.id.testText);                       // delete later
+		test.setText("testing.");                                           // delete later
 		
 		button = findViewById(R.id.gotoEventCreation);						// [BUTTON ACTION]: Event Creation Page
 		button.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +33,15 @@ public class MainActivity extends AppCompatActivity {
 				openEventCreator();
 			}
 		});
-		
+
+        goto_signin = findViewById(R.id.gotoSignIn);						// [BUTTON ACTION]: Sign In Page
+        goto_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignIn();
+            }
+        });
+
 		goto_calendar = findViewById(R.id.goto_calendar);					// [BUTTON ACTION]: Calendar View (Month)
 		goto_calendar.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -46,12 +55,16 @@ public class MainActivity extends AppCompatActivity {
 		Intent intent = new Intent(this, EventCreation.class);
 		startActivity(intent);
 	}
-	
+
 	public void open_View_Calendar() {
 		Intent intent = new Intent(this, View_Calendar.class);
 		startActivity(intent);
 	}
-	
+
+    public void openSignIn() {
+        Intent intent = new Intent(this, UserSignIn.class);
+        startActivity(intent);
+    }
 	
 }//end [ CLASS: MainActivity ]
 
--- CampusConnect/app/src/main/java/com/example/campusconnect/UserSignIn.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/UserSignIn.java
@@ -3,12 +3,49 @@ package com.example.campusconnect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class UserSignIn extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    Button signin;
+    Button gotosignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_in);
+
+
+        email= findViewById(R.id.input_email);
+        password = findViewById(R.id.input_password);
+
+        signin= findViewById(R.id.btn_login);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // call function to sign in or just do it directly from here
+            }
+        });
+
+        gotosignup= findViewById(R.id.signup);
+        gotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUserSignUp();
+            }
+        });
+
+
+    }
+
+    public void openUserSignUp() {
+        Intent intent = new Intent(this, UserSignUp.class);
+        startActivity(intent);
     }
 }
