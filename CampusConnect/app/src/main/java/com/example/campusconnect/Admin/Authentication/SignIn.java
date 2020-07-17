42d0581cd881aaf3d7e6df532519abf4948366b2
JHuraibi <fh1328@wayne.edu>
2020-07-17 13:38:50 -0400

    (Moved method)

--- CampusConnect/app/src/main/java/com/example/campusconnect/Admin/Authentication/SignIn.java
+++ CampusConnect/app/src/main/java/com/example/campusconnect/Admin/Authentication/SignIn.java
@@ -139,7 +139,24 @@ public class SignIn extends AppCompatActivity {
 			}
 			
 		});
-	}
+	
+	}// [ onCreate ]
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == RC_SIGN_IN) {
+			Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+			try {
+				GoogleSignInAccount account = task.getResult(ApiException.class);
+				firebaseAuthWithGoogle(account);
+			}
+			catch (ApiException e) {
+//				progressDialog.dismiss();
+				Toast.makeText(SignIn.this, "Google sign in failed", Toast.LENGTH_SHORT).show();
+			}
+		}
+	}// [ onActivityResult ]
 	
 	private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
 		AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
@@ -160,24 +177,9 @@ public class SignIn extends AppCompatActivity {
 						}
 					}
 				});
-	}
-	
-	
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if (requestCode == RC_SIGN_IN) {
-			Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-			try {
-				GoogleSignInAccount account = task.getResult(ApiException.class);
-				firebaseAuthWithGoogle(account);
-			}
-			catch (ApiException e) {
-//				progressDialog.dismiss();
-				Toast.makeText(SignIn.this, "Google sign in failed", Toast.LENGTH_SHORT).show();
-			}
-		}
-	}
+		
+	}// [ firebaseAuthWithGoogle ]
+
 	
 	public void gmailAccount(View view) {
 //		progressDialog = KProgressHUD.create(SignIn.this)
